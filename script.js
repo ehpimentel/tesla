caa724b1004ddc7a263aee904ae8e64934916f7f
ehpimentel <eric-pimentel@hotmail.com>
2024-09-30 00:05:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// document = ir no html
+// querySelector = busca o que eu quiser que ele busque
+
+var formulario = document.querySelector(".formulario");
+
+function aparecerFormulario(){
+    formulario.style.left = "30%";
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-50%";
+}
